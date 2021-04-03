e6b846f82b965080dde26b76bd6a8ac127291f4e
nour2297 <nourmezghaniGit22@gmail.com>
2021-04-03 09:36:50 +0100

    nou

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import myImage from "./imageInSrc.png"
+import "./style.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+    <h1 className="titleRed">Your name here</h1>
+   
+    <br />
+   
+    <img src={myImage}/>
+   
+    <br/ >
+   
+    <img src="/imageInPublic.png" />
+
+   
+   </div>
+   
+  
   );
 }
 
